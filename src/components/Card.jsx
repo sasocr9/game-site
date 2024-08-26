d51543489e0cafc7df85519755f8646061e1b0e8
Salah Hossam <salah3hossam@gmail.com>
2024-08-26 18:31:39 +0300

    final v4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Card = ({ game }) => {
-  const { id,name, cover, first_release_date: date, summary } = game;
+  const { id, name, cover, first_release_date: date, summary } = game;
   const mediumImageUrl =
     cover?.url.replace("/t_thumb/", "/t_720p/") ||
     "https://via.placeholder.com/600";
@@ -28,7 +28,7 @@ const Card = ({ game }) => {
           {summary}{" "}
         </p>
         <Link
-          to="#"
+          to={`/title/${id}`}
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Read more
